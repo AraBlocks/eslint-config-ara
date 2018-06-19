e69e6acc81ed630a67b0ff5c719b4e911686e2e5
Madeline Cameron <github@maddie.today>
2018-06-19 16:07:52 -0400

    chore(): Add rules, tighten up rules
    - Tightens up no semicolon. Throws errors when any are found.
    - Tightens up comma dangle. Throws error when comma dangle doesn't
    happen
    - Adds max line length of 80
    - Tightens up yoda. Throws error if yoda isn't used for equality
    - Adds disallowing todo and fixme comments

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 module.exports = {
   extends: "airbnb/base",
   rules: {
-    semi: "off",
+    semi: [ "error", "never" ],
     "no-plusplus": "off",
     "no-console": "warn",
     "no-magic-numbers": "off",
     "no-unused-vars": "error",
-    "comma-dangle": "off",
+    "comma-dangle": ["error", "always"],
     "eol-last": "error",
     indent: [ "error", 2 ],
-    "multiline-comment-style": [ "warn", "starred-block" ],
+    "multiline-comment-style": [ "error", "starred-block" ],
     "no-inline-comments": "error",
     eqeqeq: "warn",
+    "max-len": [
+      "warn",
+      {
+        "code": 80,
+        "comments": 100,
+        "ignoreUrls": true
+      }
+    ],
+    "valid-jsdoc": "warn",
     camelcase: "warn",
     "no-param-reassign": "warn",
     "no-use-before-define": "off",
     "no-restricted-syntax": "off",
     "guard-for-in": "off",
     "no-underscore-dangle": "off",
-    "yoda": "off",
-    "global-require": "warn"
+    "yoda": [ "error", "always", { onlyEquality: true } ],
+    "global-require": "warn",
+    "no-warning-comments": [
+      "warn",
+      {
+        "terms": [
+          "todo",
+          "fixme"
+        ],
+        "location": "anywhere"
+      }
+    ]
   }
 }
