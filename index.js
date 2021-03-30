3b49ceae475b59ded3c99a321c4bd0049c0cf5e7
Joseph Werle <joseph.werle@gmail.com>
2021-03-30 08:38:35 -0400

    chore(): upgrade everything

--- index.js
+++ index.js
@@ -1,55 +1,54 @@
 module.exports = {
-  extends: "airbnb/base",
+  extends: 'airbnb/base',
   rules: {
-    "semi": [ "error", "never" ],
-    "no-plusplus": "off",
-    "no-console": "warn",
-    "no-magic-numbers": "off",
-    "no-unused-vars": "error",
-    "comma-dangle": [ "error", "only-multiline" ],
-    "eol-last": "error",
-    "indent": [ "error", 2 ],
-    "multiline-comment-style": "off",
-    "no-inline-comments": "error",
-    "eqeqeq": "warn",
-    "max-len": [
-      "warn",
+    'semi': [ 'error', 'never' ],
+    'no-plusplus': 'off',
+    'no-console': 'warn',
+    'no-magic-numbers': 'off',
+    'no-unused-vars': 'error',
+    'comma-dangle': [ 'error', 'only-multiline' ],
+    'indent': [ 'error', 2 ],
+    'multiline-comment-style': 'off',
+    'no-inline-comments': 'error',
+    'eqeqeq': 'warn',
+    'max-len': [
+      'warn',
       {
-        "code": 80,
-        "comments": 100,
-        "ignoreUrls": true,
-        "ignoreStrings": true,
-        "ignoreTemplateLiterals": true
+        'code': 80,
+        'comments': 100,
+        'ignoreUrls': true,
+        'ignoreStrings': true,
+        'ignoreTemplateLiterals': true
       }
     ],
-    "camelcase": "warn",
-    "no-param-reassign": "off",
-    "no-use-before-define": "off",
-    "no-restricted-syntax": "off",
-    "guard-for-in": "off",
-    "no-underscore-dangle": "off",
-    "array-bracket-spacing": [ "warn", "always" ],
-    "yoda": [ "error", "always", { onlyEquality: true } ],
-    "global-require": "warn",
-    "no-warning-comments": [
-      "warn",
+    'camelcase': 'warn',
+    'no-param-reassign': 'off',
+    'no-use-before-define': 'off',
+    'no-restricted-syntax': 'off',
+    'guard-for-in': 'off',
+    'no-underscore-dangle': 'off',
+    'array-bracket-spacing': [ 'warn', 'always' ],
+    'yoda': [ 'error', 'always', { onlyEquality: true } ],
+    'global-require': 'warn',
+    'no-warning-comments': [
+      'warn',
       {
-        "terms": [
-          "todo",
-          "fixme"
+        'terms': [
+          'todo',
+          'fixme'
         ],
-        "location": "anywhere"
+        'location': 'anywhere'
       }
     ],
-    "no-void": "off",
-    "no-continue": "warn",
-    "no-multiple-empty-lines": [
-      "error", {
-        "max": 1,
-        "maxEOF": 1,
-        "maxBOF": 0
+    'no-void': 'off',
+    'no-continue': 'warn',
+    'no-multiple-empty-lines': [
+      'error', {
+        'max': 1,
+        'maxEOF': 1,
+        'maxBOF': 0
       }
     ],
-    "eol-last": [ "error", "always" ]
+    'eol-last': [ 'error', 'always' ]
   }
 }
